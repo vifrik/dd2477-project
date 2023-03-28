185aa9a36b619affc8c4ff83c3d382e53e7180d0
Viktor Frideen <frideen.viktor@gmail.com>
2023-03-28 19:59:16 +0200

    Make sample java more complicated

--- other/sample_java/MyClass.java
+++ other/sample_java/MyClass.java
@@ -1,5 +1,13 @@
 public class MyClass {
+  private static final int CONSTANT = 5;
   int n;
   public void MyFunction(int n) { this.n = n; }
-  public int getN() { return n; }
+  public int getN() {
+    String hello = "hello";
+    Integer some_type = 38;
+    int x = 3;
+    int y = 2 * x;
+    System.out.println(y);
+    return n;
+  }
 }
