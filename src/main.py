9185ec97a36e1ce470f930ab2142946b7972c624
Viktor Frideen <frideen.viktor@gmail.com>
2023-03-29 23:51:01 +0200

    Change main.py

--- src/main.py
+++ src/main.py
@@ -7,26 +7,34 @@ from parser import Parser
 from credentials import token
 
 
-def scrape():
+def scrape(args):
     crawler = Scraper(token)
     crawler.run()
 
 
-def parse():
+def parse(args):
     parser = Parser("data/java", "metadata.json")
     for json_object in parser.parse_folder():
-        upload(json_object)
+        if args.upload:
+            upload(args, json_object)
+        else:
+            print(json_object)
 
 
-def upload(json_object):
-    pass
+def upload(args, json_object):
+    if args.delete:
+        pass  # TODO: delete here
+
+    pass  # TODO: upload here
 
 
 def parse_args():
     Parser = argparse.ArgumentParser()
     mode_group = Parser.add_mutually_exclusive_group(required=True)
-    mode_group.add_argument("-s", "--scrape", help="should scraper", action="store_true")
-    mode_group.add_argument("-p", "--parse", help="should parse", action="store_true")
+    mode_group.add_argument("-S", "--scrape", help="should scraper", action="store_true")
+    mode_group.add_argument("-P", "--parse", help="should parse", action="store_true")
+    Parser.add_argument("-u", "--upload", help="should upload", action="store_true")
+    Parser.add_argument("--delete", help="(WARNING, deletes entire index) should delete", action="store_true")
     return Parser.parse_args()
 
 
@@ -34,12 +42,9 @@ def main():
     args = parse_args()
 
     if args.scrape:
-        scrape()
-        exit(0)
-
-    if args.parse:
-        parse()
-        exit(0)
+        scrape(args)
+    elif args.parse:
+        parse(args)
 
 
 if __name__ == '__main__':
