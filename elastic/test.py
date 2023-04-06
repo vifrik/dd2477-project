2cb3255ec04a9505608b10f3cec6adb9e2def02a
Pierre Le Fevre <hello@pierrelf.com>
2023-04-06 13:00:46 +0200

    Add auth

--- elastic/test.py
+++ elastic/test.py
@@ -1,21 +1,30 @@
 from datetime import datetime
 from elasticsearch import Elasticsearch
-es = Elasticsearch('https://elastic.åt.se:443')
 
-doc = {
-    'author': 'author_name',
-    'text': 'Interesting content...',
-    'timestamp': datetime.now(),
-}
-resp = es.index(index="test-index", document=doc)
-print(resp['result'])
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# Get document
-resp = es.get(index="test-index", id=resp['_id'])
-print(resp['_source'])
+USER = os.getenv('AUTH_USER')
+PASSWORD = os.environ.get('AUTH_PASSWORD')
 
-# Refresh index
-es.indices.refresh(index="test-index")
+es = Elasticsearch('https://elastic.åt.se:443',
+                   basic_auth=(USER, PASSWORD))
+
+# doc = {
+#     'author': 'author_name',
+#     'text': 'Interesting content...',
+#     'timestamp': datetime.now(),
+# }
+# resp = es.index(index="test-index", document=doc)
+# print(resp['result'])
+
+# # Get document
+# resp = es.get(index="test-index", id=resp['_id'])
+# print(resp['_source'])
+
+# # Refresh index
+# es.indices.refresh(index="test-index")
 
 # Search
 resp = es.search(index="test-index", query={"match_all": {}})
