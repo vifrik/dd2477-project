3cddc5af0a536e99d9176e57c799a8551db49470
Viktor Frideen <frideen.viktor@gmail.com>
2023-04-09 01:44:14 +0200

    Make query_dsl pip compatible

---
+++ query_dsl/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Query DSL",
+    version="0.0.1",
+    description="Query DSL lexer and parser for course DD2477 @ KTH",
+    author="Viktor Frideen",
+    author_email="frideen@kth.se",
+    packages=["query_dsl"]
+)
