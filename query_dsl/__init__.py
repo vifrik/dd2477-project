9ffe8590907bf93fa03eba2ab6a60686510936d3
Viktor Frideen <frideen.viktor@gmail.com>
2023-04-13 00:03:19 +0200

    Change query_dsl version

--- query_dsl/__init__.py
+++ query_dsl/__init__.py
@@ -1,4 +1,4 @@
 from .query_dsl import lexer
 from .query_dsl import parser
 
-__version__ = "0.0.1"
+__version__ = "0.0.11"
--- query_dsl/setup.py
+++ query_dsl/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Query DSL",
-    version="0.0.1",
+    version="0.0.11",
     description="Query DSL lexer and parser for course DD2477 @ KTH",
     author="Viktor Frideen",
     author_email="frideen@kth.se",
