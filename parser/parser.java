839e7995688301c290b4003237c6a443c724f9af
Viktor Frideen <frideen.viktor@gmail.com>
2023-04-24 17:12:21 +0200

    Update parser.java

--- parser/parser.java
+++ parser/parser.java
@@ -2,6 +2,7 @@ import com.github.javaparser.ParseProblemException;
 import com.github.javaparser.Range;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import org.json.JSONArray;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public class Main {
 
     private static class JsonEntry extends VoidVisitorAdapter<Void> {
+
         JSONArray jsonArray = new JSONArray();
 
         public JSONArray getJson() {
@@ -33,6 +35,7 @@ public class Main {
     }
 
     private static class ClassOrInterfaceVisitor extends JsonEntry {
+
         @Override
         public void visit(ClassOrInterfaceDeclaration classDeclaration, Void arg) {
             JSONObject jsonObject = new JSONObject();
@@ -40,6 +43,15 @@ public class Main {
             jsonObject.put("name", classDeclaration.getNameAsString());
             jsonObject.put("name_position", getPositionJson(classDeclaration.getName().getRange()));
             jsonObject.put("position", getPositionJson(classDeclaration.getRange()));
+
+            JSONArray extends_list = new JSONArray();
+            classDeclaration.getExtendedTypes().forEach(extended -> {
+                JSONObject entry = new JSONObject();
+                entry.put("name", extended.getName());
+                entry.put("position", getPositionJson(extended.getRange()));
+                extends_list.put(entry);
+            });
+            jsonObject.put("extends", extends_list);
 
             JSONArray modifiers = new JSONArray();
             classDeclaration.getModifiers().forEach(modifier -> {
@@ -59,20 +71,22 @@ public class Main {
             });
             jsonObject.put("annotations", annotations);
 
-
             jsonArray.put(jsonObject);
         }
     }
 
     private static class MethodNameVisitor extends JsonEntry {
+
         @Override
         public void visit(MethodDeclaration methodDeclaration, Void arg) {
             JSONObject jsonObject = new JSONObject();
 
             jsonObject.put("name", methodDeclaration.getNameAsString());
-            jsonObject.put("name_position", getPositionJson(methodDeclaration.getName().getRange()));
+            jsonObject.put("name_position",
+                getPositionJson(methodDeclaration.getName().getRange()));
             jsonObject.put("return_type", methodDeclaration.getTypeAsString());
-            jsonObject.put("return_type_position", getPositionJson(methodDeclaration.getType().getRange()));
+            jsonObject.put("return_type_position",
+                getPositionJson(methodDeclaration.getType().getRange()));
             jsonObject.put("position", getPositionJson(methodDeclaration.getRange()));
 
             JSONArray modifiers = new JSONArray();
@@ -88,6 +102,12 @@ public class Main {
             methodDeclaration.getParameters().forEach(parameter -> {
                 JSONObject entry = new JSONObject();
                 entry.put("name", parameter.getNameAsString());
+                entry.put("name_position",
+                    getPositionJson(parameter.getName().getRange()));
+                entry.put("type", parameter.getTypeAsString());
+                entry.put("type_position",
+                    getPositionJson(parameter.getType().getRange()));
+
                 entry.put("position", getPositionJson(parameter.getRange()));
                 parameters.put(entry);
             });
@@ -102,18 +122,23 @@ public class Main {
             });
             jsonObject.put("annotations", annotations);
 
-
             jsonArray.put(jsonObject);
         }
     }
 
     private static class FieldVisitorAdapter extends JsonEntry {
+
         @Override
         public void visit(FieldDeclaration fieldDeclaration, Void arg) {
             JSONObject jsonObject = new JSONObject();
 
             jsonObject.put("name", fieldDeclaration.getVariable(0).getNameAsString());
-            jsonObject.put("name_position", getPositionJson(fieldDeclaration.getVariable(0).getName().getRange()));
+            jsonObject.put("name_position",
+                getPositionJson(fieldDeclaration.getVariable(0).getRange()));
+            jsonObject.put("type", fieldDeclaration.getVariable(0).getTypeAsString());
+            jsonObject.put("type_position",
+                getPositionJson(fieldDeclaration.getVariable(0).getType().getRange()));
+
             jsonObject.put("position", getPositionJson(fieldDeclaration.getRange()));
 
             JSONArray modifiers = new JSONArray();
@@ -130,12 +155,18 @@ public class Main {
     }
 
     private static class VariableNameVisitor extends JsonEntry {
+
         @Override
         public void visit(VariableDeclarator variableDeclarator, Void arg) {
             JSONObject jsonObject = new JSONObject();
 
             jsonObject.put("name", variableDeclarator.getNameAsString());
-            jsonObject.put("name_position", getPositionJson(variableDeclarator.getName().getRange()));
+            jsonObject.put("name_position",
+                getPositionJson(variableDeclarator.getName().getRange()));
+            jsonObject.put("type", variableDeclarator.getTypeAsString());
+            jsonObject.put("type_position",
+                getPositionJson(variableDeclarator.getType().getRange()));
+
             jsonObject.put("position", getPositionJson(variableDeclarator.getRange()));
 
             jsonArray.put(jsonObject);
@@ -144,6 +175,28 @@ public class Main {
 
     private static JSONObject parseFile(String filepath) throws FileNotFoundException {
         CompilationUnit cu = StaticJavaParser.parse(new File(filepath));
+
+        JSONObject metadata = new JSONObject();
+
+        JSONArray imports = new JSONArray();
+        cu.getImports().forEach(import_ -> {
+            JSONObject entry = new JSONObject();
+            entry.put("name", import_.getNameAsString());
+            entry.put("name_position", getPositionJson(import_.getName().getRange()));
+            entry.put("position", getPositionJson(import_.getRange()));
+            imports.put(entry);
+        });
+        metadata.put("imports", imports);
+
+        String package_string = "";
+        JSONArray positionJson = new JSONArray();
+        Optional<PackageDeclaration> pkg = cu.getPackageDeclaration();
+        if (pkg.isPresent()) {
+            package_string = pkg.get().getNameAsString();
+            positionJson = getPositionJson(pkg.get().getRange());
+        }
+        metadata.put("package", package_string);
+        metadata.put("package_position", positionJson);
 
         MethodNameVisitor methodNamesVisitor = new MethodNameVisitor();
         methodNamesVisitor.visit(cu, null);
@@ -158,6 +211,7 @@ public class Main {
         variableNameVisitor.visit(cu, null);
 
         JSONObject result = new JSONObject();
+        result.put("metadata", metadata);
         result.put("classes", classOrInterfaceVisitor.getJson());
         result.put("methods", methodNamesVisitor.getJson());
         result.put("fields", fieldVisitorAdapter.getJson());
@@ -166,7 +220,8 @@ public class Main {
         return result;
     }
 
-    private static JSONArray parseFolder(String folderpath, String metadataPath) throws IOException {
+    private static JSONArray parseFolder(String folderpath, String metadataPath)
+        throws IOException {
         JSONArray result = new JSONArray();
 
         File metadata = new File(metadataPath);
@@ -183,7 +238,12 @@ public class Main {
                 if (file.getName().endsWith(".java")) {
                     try {
                         JSONObject partialResult = parseFile(file.getAbsolutePath());
-                        partialResult.put("metadata", metadataJson.get(file.getName()));
+                        JSONObject fileContents = (JSONObject) metadataJson.get(file.getName());
+                        JSONObject metadata_temp = (JSONObject) partialResult.get("metadata");
+                        for (String key : JSONObject.getNames(fileContents)) {
+                            metadata_temp.put(key, fileContents.get(key));
+                        }
+                        partialResult.put("metadata", metadata_temp);
                         result.put(partialResult);
                     } catch (FileNotFoundException e) {
                         System.err.printf("file: %s not found%n", file.getAbsolutePath());
